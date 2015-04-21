588e8e976be45e3b19227b2944118493e7051ada
Yoshiya Hinosawa <stibium121@gmail.com>
2015-04-21 23:08:22 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,88 @@
+# custom-class.js v0.1.0
+
+> define a special class with js ability
+
+***note*** This library depends on jQuery.
+
+
+## Doc
+
+```js
+/**
+ * @param {String} name The name
+ * @param {Function} definingFunction The defining function
+ */
+$.registerCustomClass(name, definingFunction);
+```
+
+This library expose the function `$.registerCustomClass(name, definingFunction)`.
+
+This registers a "custom class" of the given name using the given defining function.
+You can define the behaviour of the custom class by the defining function. `this` of the defining function scope is the custome class element itself.
+
+This automatically initializes all custom class elements on the page at `$(document).ready` timing.
+
+This also registers `init.class-name` event handler to `document`, which invokes the initialization of the class,
+so if you want to initialize them after `$(document).ready`, you need to trigger `init.class-name` event on the document.
+
+The initialization doesn't run over twice for a element.
+
+## Examples
+
+```
+<script>
+
+$.registerCustomClass('go-to-example-com', function () {
+
+    $(this).click(function () {
+
+        location.href = 'http://example.com/';
+
+    });
+
+});
+
+</script>
+
+<div class="go-to-example-com">...</div>
+```
+
+When you click the above div, the page go to the example.com
+
+----
+
+```html
+<script>
+
+$.registerCustomClass('my-anchor', function () {
+
+    var self = $(this); //
+
+    self.on('click', function () {
+
+        location.href = $(this).attr('href');
+
+    });
+
+    self.on('mouseover', function () {
+
+        $(this).addClass('hover');
+
+    });
+
+    self.on('mouseout', function () {
+
+        $(this).removeClass('hover');
+
+    });
+
+});
+
+</script>
+
+<div class="my-anchor" href="https://www.google.com/">...</div>
+```
+
+This `.my-anchor` class is similar to `<a>`.
+
+When you click the above div, the page goes to href's url (https://www.google.com/ in this case) and when you mouse over it, it gets `.hover` class.
---
+++ custom-class.js
@@ -0,0 +1,182 @@
+/**
+ * custom-class.js v0.1.0
+ * author: Yoshiya Hinosawa ( http://github.com/kt3k )
+ * license: MIT
+ */
+
+
+(function ($) {
+    'use strict';
+
+    if (typeof $ === 'undefined') {
+
+        console.log('error: jQuery unavailable');
+        console.log('custom-class.js depends on jQuery. load jQuery in global scope before this library.');
+
+        return;
+
+    }
+
+
+    /**
+     Registers a custom class of the given name using the given defining function.
+
+     This automatically initializes all `.class-name` elements on the page at `$(document).ready` timing.
+
+     This also registers `init.class-name` event handler to `document`, which invokes the initialization of the class,
+     so if you want to initialize them after `$(document).ready`, you need to trigger `init.class-name` event on the document.
+
+     The initialization doesn't run over twice for a element.
+
+     @param {String} className The class name
+     @param {Function} definition The class definition
+
+     @example
+
+        $.registerCustomClass('go-to-example-com', function () {
+
+            $(this).click(function () {
+
+                location.href = 'http://example.com/';
+
+            });
+
+        });
+
+        // <div class="go-to-example-com">...</div>
+        //
+        // When you click the above div, the page go to the example.com
+         
+
+     @example
+
+        $.registerCustomClass('my-anchor', function () {
+
+            var self = $(this); //
+
+            self.on('click', function () {
+
+                location.href = $(this).attr('href');
+
+            });
+
+            self.on('mouseover', function () {
+
+                $(this).addClass('hover');
+
+            });
+
+            self.on('mouseout', function () {
+
+                $(this).removeClass('hover');
+
+            });
+
+        });
+
+        // `.my-anchor` is similar to <a>
+        //
+        // <div class="my-anchor" href="https://www.google.com/">...</div>
+        // 
+        // When you click the above, the page goes to href's url (https://www.google.com/ in this case).
+        // When you mouse over the above it gets `.hover` class.
+
+     */
+    $.registerCustomClass = function (name, definingFunction) {
+        'use strict';
+
+        var customClass = new CustomClass(name, definingFunction);
+
+        var init = function () {
+
+            $(customClass.selector()).each(function () {
+
+                customClass.markInitialized(this);
+
+                customClass.applyCustomDefinition(this);
+
+            });
+
+        };
+
+
+        $(document).on(customClass.initEvent(), init);
+
+        $(document).ready(function () {
+
+            $(document).trigger(customClass.initEvent());
+
+        });
+
+    };
+
+
+
+    /**
+     CustomClass is the utility class for custom class initialization.
+     */
+    function CustomClass(className, definingFunction) {
+
+        this.className = className;
+        this.definingFunction = definingFunction;
+
+    };
+
+    var pt = CustomClass.prototype;
+
+
+    /**
+     Returns init event string.
+
+     @return {String}
+     */
+    pt.initEvent = function () {
+
+        return 'init.' + this.className;
+
+    }
+
+    /**
+     @private
+     @return {String}
+     */
+    pt.initializedClass = function () {
+
+        return this.className + '-initialized';
+
+    }
+
+    /**
+     Returns the selector for uninitialized custom class.
+
+     @return {String}
+     */
+    pt.selector = function () {
+
+        return '.' + this.className + ':not(.' + this.initializedClass() + ')';
+
+    };
+
+    /**
+     Marks the given element as initialized as this custom class.
+
+     @param {HTMLElement} elem
+     */
+    pt.markInitialized = function (elem) {
+
+        $(elem).addClass(this.initializedClass());
+
+    };
+
+    /**
+     Applies the defining function to the element.
+
+     @param {HTMLElement} elem
+     */
+    pt.applyCustomDefinition = function (elem) {
+
+        this.definingFunction.call(elem)
+
+    };
+
+}(window.jQuery));
