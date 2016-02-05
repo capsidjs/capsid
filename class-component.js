d4d72e8962344139de67b4ea1fc49b2fa629ffa6
Yoshiya Hinosawa <stibium121@gmail.com>
2016-02-05 18:23:41 +0900

    Rename decorator, manage double loading

--- class-component.js
+++ class-component.js
@@ -18,138 +18,153 @@ var ClassComponentManager = require('./lib/ClassComponentManager');
 require('./lib/fn.cc');
 
 /**
- * The main namespace for class component module.
+ * Creats the module object.
+ *
+ * @return {Object}
  */
-var cc = {};
+var createModuleObject = function () {
 
-cc.__manager__ = new ClassComponentManager();
+    /**
+     * The main namespace for class component module.
+     */
+    var cc = {};
 
-/**
- Registers a class component of the given name using the given defining function.
+    cc.__manager__ = new ClassComponentManager();
 
- See README.md for details.
+    /**
+     Registers a class component of the given name using the given defining function.
 
- @param {String} className The class name
- @param {Function} definingFunction The class definition
- */
-cc.register = function (name, definingFunction) {
+     See README.md for details.
 
-    if (typeof name !== 'string') {
+     @param {String} className The class name
+     @param {Function} definingFunction The class definition
+     */
+    cc.register = function (name, definingFunction) {
 
-        throw new Error('`name` of a class component has to be a string');
+        if (typeof name !== 'string') {
 
-    }
+            throw new Error('`name` of a class component has to be a string');
 
-    if (typeof definingFunction !== 'function') {
+        }
 
-        throw new Error('`definingFunction` of a class component has to be a function');
+        if (typeof definingFunction !== 'function') {
 
-    }
+            throw new Error('`definingFunction` of a class component has to be a function');
 
-    cc.__manager__.register(name, definingFunction);
+        }
 
-
-    $(document).ready(function () {
-
-        cc.__manager__.init(name);
-
-    });
-
-};
+        cc.__manager__.register(name, definingFunction);
 
 
-/**
- * Initialized the all class components of the given names and returns of the promise of all initialization.
- *
- * @param {String[]|String} arguments
- * @return {Object<HTMLElement[]>}
- */
-cc.init = function (classNames, elem) {
+        $(document).ready(function () {
 
-    if (classNames == null) {
+            cc.__manager__.init(name);
 
-        cc.__manager__.initAll(elem);
-
-        return;
-
-    }
-
-    if (typeof classNames === 'string') {
-
-        classNames = classNames.split(reSpaces);
-
-    }
-
-    return classNames.map(function (className) {
-
-        return cc.__manager__.init(className, elem);
-
-    });
-
-};
-
-
-/**
- * Assign a class as the accompanying coelement of the class component
- *
- * @param {String} className
- * @param {Function} DefiningClass
- */
-cc.assign = function (className, DefiningClass) {
-
-    DefiningClass.coelementName = className;
-
-    cc.register(className, function (elem) {
-
-        var coelement = new DefiningClass(elem);
-
-        elem.data('__coelement:' + DefiningClass.coelementName, coelement);
-
-    });
-
-};
-
-/**
- * The decorator for class assignment.
- *
- * @example
- *   @Component('foo')
- *   class Foo extends Bar {
- *     ...
- *   }
- *
- * The above is the same as:
- *
- * @example
- *   class Foo extends Bar {
- *   }
- *
- *   $.cc.assign('foo', Foo)
- *
- * @param {String} className The class name
- * @return {Function}
- */
-cc.Component = function (className) {
-
-    // This is the actual decorator
-    return function (Cls) {
-
-        cc.assign(className, Cls)
+        });
 
     };
 
+
+    /**
+     * Initialized the all class components of the given names and returns of the promise of all initialization.
+     *
+     * @param {String[]|String} arguments
+     * @return {Object<HTMLElement[]>}
+     */
+    cc.init = function (classNames, elem) {
+
+        if (classNames == null) {
+
+            cc.__manager__.initAll(elem);
+
+            return;
+
+        }
+
+        if (typeof classNames === 'string') {
+
+            classNames = classNames.split(reSpaces);
+
+        }
+
+        return classNames.map(function (className) {
+
+            return cc.__manager__.init(className, elem);
+
+        });
+
+    };
+
+
+    /**
+     * Assign a class as the accompanying coelement of the class component
+     *
+     * @param {String} className
+     * @param {Function} DefiningClass
+     */
+    cc.assign = function (className, DefiningClass) {
+
+        DefiningClass.coelementName = className;
+
+        cc.register(className, function (elem) {
+
+            var coelement = new DefiningClass(elem);
+
+            elem.data('__coelement:' + DefiningClass.coelementName, coelement);
+
+        });
+
+    };
+
+    /**
+     * The decorator for class assignment.
+     *
+     * @example
+     *   @$.cc.component('foo')
+     *   class Foo extends Bar {
+     *     ...
+     *   }
+     *
+     * The above is the same as:
+     *
+     * @example
+     *   class Foo extends Bar {
+     *   }
+     *
+     *   $.cc.assign('foo', Foo)
+     *
+     * @param {String} className The class name
+     * @return {Function}
+     */
+    cc.component = function (className) {
+
+        // This is the actual decorator
+        return function (Cls) {
+
+            cc.assign(className, Cls)
+
+        };
+
+    };
+
+    // Exports subclass.
+    cc.subclass = subclass;
+
+    // Exports Actor.
+    cc.Actor = Actor;
+
+    // Exports Actor.
+    cc.Coelement = Coelement;
+
+    return cc;
+
 };
 
-// Exports subclass.
-cc.subclass = subclass;
+// If the cc is not set, then create one.
+if ($.cc == null) {
 
-// Exports Actor.
-cc.Actor = Actor;
+    $.cc = createModuleObject()
 
-// Exports Actor.
-cc.Coelement = Coelement;
+}
 
-// Exports the main namespace
-$.cc = cc;
-
-module.exports = cc;
+module.exports = $.cc;
