34b658caabb23031b679cf7a78632091b2fb3226
Yoshiya Hinosawa <stibium121@gmail.com>
2016-05-04 10:54:16 +0900

    Simplify karma settings

--- karma.conf.js
+++ karma.conf.js
@@ -2,19 +2,14 @@ module.exports = function(config) {
   config.set({
     frameworks: ['browserify', 'mocha'],
     files: ['class-component-spec.js'],
-    preprocessors: {
-      'class-component-spec.js': ['browserify']
-    },
+    preprocessors: {'class-component-spec.js': 'browserify'},
     browserify: {
       debug: true,
       transform: [require('browserify-istanbul')({ignore: ['**/node_modules/**', '**/*-spec.js']})]
     },
     reporters: ['progress', 'coverage'],
-    coverageReporter: {
-      reporters: [{type: 'html'}, {type: 'lcov'}]
-    },
+    coverageReporter: {type: 'lcov'},
     port: 9876,
-    colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
