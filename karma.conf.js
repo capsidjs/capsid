1142f974e04ac7225a15d4b60abd043be258865f
Yoshiya Hinosawa <stibium121@gmail.com>
2016-05-04 11:22:54 +0900

    Lint karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,18 +1,18 @@
-module.exports = function(config) {
-  config.set({
-    frameworks: ['browserify', 'mocha'],
-    files: ['class-component-spec.js'],
-    preprocessors: {'class-component-spec.js': 'browserify'},
-    browserify: {
-      debug: true,
-      transform: [require('browserify-istanbul')({ignore: ['**/node_modules/**', '**/*-spec.js']})]
-    },
-    reporters: ['progress', 'coverage'],
-    coverageReporter: {type: 'lcov'},
-    port: 9876,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: true
-  })
+module.exports = function (config) {
+    config.set({
+        frameworks: ['browserify', 'mocha'],
+        files: ['class-component-spec.js'],
+        preprocessors: {'class-component-spec.js': 'browserify'},
+        browserify: {
+            debug: true,
+            transform: [require('browserify-istanbul')({ignore: ['**/node_modules/**', '**/*-spec.js']})]
+        },
+        reporters: ['progress', 'coverage'],
+        coverageReporter: {type: 'lcov'},
+        port: 9876,
+        logLevel: config.LOG_INFO,
+        autoWatch: true,
+        browsers: ['Chrome'],
+        singleRun: true
+    })
 }
