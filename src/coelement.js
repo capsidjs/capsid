6424987d6a43418b545af1d33b0935dd8350c5e4
Yoshiya Hinosawa <stibium121@gmail.com>
2016-05-26 20:08:32 +0900

    beaking(coelement): remove coelement's methods

--- src/coelement.js
+++ src/coelement.js
@@ -8,36 +8,6 @@ class Coelement {
   constructor(elem) {
     this.elem = elem
   }
-
-  /**
-   * Binds the event listener to the given event.
-   * @param {string} event The event
-   * @param {string} selector The selector
-   * @param {Function} listener The listener
-   * @return {Coelement}
-   */
-  on(event, selector, listener) {
-    this.elem.on(event, selector, listener)
-  }
-
-  /**
-   * Unbinds the event
-   * @param {string} event The event
-   * @param {string} selector The selector
-   * @param {Function} listener The listener
-   */
-  off(event, selector) {
-    this.elem.off(event, selector, listener)
-  }
-
-  /**
-   * Triggers the event.
-   * @param {string} event The event
-   * @param {object} data The event parameter
-   */
-  trigger(event, data) {
-    this.elem.trigger(event, data)
-  }
 }
 
 module.exports = Coelement
