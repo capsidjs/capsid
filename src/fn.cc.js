3be5eae2841671b77d27f5fd2ff5e269d53855c7
Yoshiya Hinosawa <stibium121@gmail.com>
2016-08-20 17:31:19 +0900

    refactor(fn.cc): refactor this handling

--- src/fn.cc.js
+++ src/fn.cc.js
@@ -5,15 +5,16 @@ const CLASS_COMPONENT_DATA_KEY = '__class_component_data__'
 // Defines the special property cc on the jquery prototype.
 export default $ => Object.defineProperty($.fn, 'cc', {
   get () {
-    let cc = this.data(CLASS_COMPONENT_DATA_KEY)
+    const self = this
+    let cc = self.data(CLASS_COMPONENT_DATA_KEY)
 
     if (!cc) {
-      this.data(CLASS_COMPONENT_DATA_KEY, cc = classNames => {
-        componentInit(this, classNames)
-        return this
+      self.data(CLASS_COMPONENT_DATA_KEY, cc = classNames => {
+        componentInit(self, classNames)
+        return self
       })
 
-      cc.get = className => componentGet(this, className)
+      cc.get = className => componentGet(self, className)
       cc.init = className => cc(className).cc.get(className)
     }
 
