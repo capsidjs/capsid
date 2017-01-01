5070cef961f994e8aeda3bd3fd78dce09a6f854a
Yoshiya Hinosawa <stibium121@gmail.com>
2017-01-01 15:32:13 +0900

    :memo: fix(event-trigger): fix flow annotations

--- src/event-trigger.js
+++ src/event-trigger.js
@@ -1,5 +1,10 @@
+// @flow
 /**
+ * Triggers the event.
+ * @param el The element
+ * @param type The event type
+ * @param detail The optional detail object
  */
-export default (el: HTMLElement, type: string, detail?: any) => {
+export default (el: HTMLElement, type: string, detail?: any): void => {
   el.dispatchEvent(new CustomEvent(type, { detail }))
 }
