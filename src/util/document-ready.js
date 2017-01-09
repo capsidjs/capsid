a14de1cfc28dbd3939c54d01e6163f0c080e4fdd
Yoshiya Hinosawa <stibium121@gmail.com>
2017-01-09 16:27:48 +0900

    :hammer: refactor(document-ready): remove load event handling because DOMContentLoaded is always available

--- src/util/document-ready.js
+++ src/util/document-ready.js
@@ -3,20 +3,17 @@
 import doc from './document.js'
 
 const DOM_CONTENT_LOADED = 'DOMContentLoaded'
-const LOAD = 'load'
 
 const promise = new Promise(resolve => {
   const completed = () => {
     resolve()
     doc.removeEventListener(DOM_CONTENT_LOADED, completed)
-    doc.removeEventListener(LOAD, completed)
   }
 
   if (doc.readyState === 'complete') {
     setTimeout(resolve)
   } else {
     doc.addEventListener(DOM_CONTENT_LOADED, completed)
-    doc.addEventListener(LOAD, completed)
   }
 })
 
