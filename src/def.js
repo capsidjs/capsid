3c9f1ce61ec5db0bbb35bb47337760ff6be825dd
Yoshiya Hinosawa <stibium121@gmail.com>
2017-01-12 00:01:00 +0900

    refactor(def): inline create-component-initializer

--- src/def.js
+++ src/def.js
@@ -1,9 +1,10 @@
 // @flow
-import createComponentInitializer from './create-component-initializer.js'
 import check from './util/check.js'
 import { ready } from './util/document'
 import ccc from './ccc.js'
 import init from './init.js'
+import plugins from './plugins.js'
+import { COELEMENT_DATA_KEY_PREFIX, KEY_EVENT_LISTENERS } from './const.js'
 
 /**
  * Registers the class-component for the given name and constructor and returns the constructor.
@@ -15,7 +16,40 @@ const def: any = (name: string, Constructor: Function): Function => {
   check(typeof name === 'string', '`name` of a class component has to be a string.')
   check(typeof Constructor === 'function', '`Constructor` of a class component has to be a function')
 
-  ccc[name] = createComponentInitializer(name, Constructor)
+  const initClass = `${name}-initialized`
+
+  /**
+   * Initializes the html element by the configuration.
+   * @param el The html element
+   * @param coelem The dummy parameter, don't use
+   */
+  const initializer = (el: HTMLElement, coelem: Constructor) => {
+    const classList = el.classList
+
+    if (!classList.contains(initClass)) {
+      (el: any)[COELEMENT_DATA_KEY_PREFIX + name] = coelem = new Constructor()
+
+      plugins.forEach(plugin => {
+        plugin(el, coelem)
+      })
+
+      coelem.el = el
+
+      if (typeof coelem.__init__ === 'function') {
+        coelem.__init__()
+      }
+
+      (Constructor[KEY_EVENT_LISTENERS] || []).map(listenerBinder => {
+        listenerBinder(el, coelem)
+      })
+
+      classList.add(initClass)
+    }
+  }
+
+  initializer.selector = `.${name}:not(.${initClass})`
+
+  ccc[name] = initializer
 
   ready(() => { init(name) })
 
