0b11c5585794c13042b11f790bf23d18e7c7dfb4
Yoshiya Hinosawa <stibium121@gmail.com>
2017-07-11 15:20:51 +0900

    feat: add @on.click

--- src/decorators/index.js
+++ src/decorators/index.js
@@ -1,4 +1,9 @@
-export { default as on } from './on.js'
+import onClick from './on.click.js'
+import on from './on.js'
+
+on.click = onClick
+
+export { on }
 export { default as emit } from './emit.js'
 export { default as wire } from './wire.js'
 export { default as component } from './component.js'
