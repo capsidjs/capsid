d280530797bbdf4e32d6da58b1c0ee65342ec7dc
Yoshiya Hinosawa <stibium121@gmail.com>
2018-03-08 10:12:13 +0900

    :sparkles: feat: add install method

---
+++ src/install.js
@@ -0,0 +1,21 @@
+// @flow
+
+import * as capsid from './index.js'
+
+type CapsidModule = {
+  install: Function
+}
+
+/**
+ * Installs the capsid module or plugin.
+ *
+ * @param {CapsidModule} capsidModule
+ * @param {object} options
+ */
+export default (capsidModule: CapsidModule, options: Object) => {
+  if (capsidModule.install) {
+    throw new Error('The given capsid module does not have `install` method. Please check the install call.')
+  }
+
+  capsidModule.install(capsid, options || {})
+}
