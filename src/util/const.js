d900e0493aaf5c95d017adea43b8d741aab3052f
Yoshiya Hinosawa <stibium121@gmail.com>
2018-05-20 06:08:25 +0900

    refactor: change key names(private)

--- src/util/const.js
+++ src/util/const.js
@@ -1,5 +1,5 @@
 // @flow
-export const COELEMENT_DATA_KEY_PREFIX = '$D'
-export const KEY_EVENT_LISTENERS = '$K'
-export const INITIALIZED_KEY = '$I'
-export const COMPONENT_NAME_KEY = '$N'
+export const COELEMENT_DATA_KEY_PREFIX = 'C$'
+export const KEY_EVENT_LISTENERS = 'K$'
+export const INITIALIZED_KEY = 'I$'
+export const COMPONENT_NAME_KEY = 'N$'
