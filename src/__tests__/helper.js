a75e7f4659c2916006d036c59c961d4da60b56ce
Yoshiya Hinosawa <stibium121@gmail.com>
2018-11-21 14:41:08 +0900

    refactor: remove unused fixture

--- src/__tests__/helper.js
+++ src/__tests__/helper.js
@@ -1,14 +1,8 @@
 // @flow
 
 import * as capsid from '../index.js'
-import { Foo, Bar } from './fixture.js'
 
 global.__DEV__ = true
-
-before(() => {
-  capsid.def('foo', Foo)
-  capsid.def('bar', Bar)
-})
 
 export const clearComponents = () =>
   Object.keys(capsid.__ccc__).forEach(key => {
