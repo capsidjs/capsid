f5f57462cd2931c9a53e53cd9a23fafee777380c
Yoshiya Hinosawa <stibium121@gmail.com>
2018-12-01 22:35:05 +0900

    :bookmark: chore(version): bump to v0.29.0

--- dist/capsid-outside-events.js
+++ dist/capsid-outside-events.js
@@ -5,40 +5,29 @@
 }(this, (function () { 'use strict';
 
 var KEY_OUTSIDE_EVENT_LISTENERS = '#O';
-
-var install = function install(capsid) {
-  var on = capsid.on,
-      pluginHooks = capsid.pluginHooks;
-
-  on.outside = function (event) {
-    return function (descriptor) {
-      var key = descriptor.key;
-
-      descriptor.finisher = function (constructor) {
-        constructor[KEY_OUTSIDE_EVENT_LISTENERS] = (constructor[KEY_OUTSIDE_EVENT_LISTENERS] || []).concat(function (el, coelem) {
-          var listener = function listener(e) {
-            if (el !== e.target && !el.contains(e.target)) {
-              coelem[key](e);
-            }
-          };
-
-          document.addEventListener(event, listener);
+var install = function (capsid) {
+    var on = capsid.on, pluginHooks = capsid.pluginHooks;
+    on.outside = function (event) { return function (descriptor) {
+        var key = descriptor.key;
+        descriptor.finisher = function (constructor) {
+            constructor[KEY_OUTSIDE_EVENT_LISTENERS] = (constructor[KEY_OUTSIDE_EVENT_LISTENERS] || []).concat(function (el, coelem) {
+                var listener = function (e) {
+                    if (el !== e.target && !el.contains(e.target)) {
+                        coelem[key](e);
+                    }
+                };
+                document.addEventListener(event, listener);
+            });
+        };
+    }; };
+    pluginHooks.push(function (el, coelem) {
+        
+        (coelem.constructor[KEY_OUTSIDE_EVENT_LISTENERS] || []).map(function (eventListenerBinder) {
+            eventListenerBinder(el, coelem);
         });
-      };
-    };
-  };
-
-  pluginHooks.push(function (el, coelem) {
-    
-    (coelem.constructor[KEY_OUTSIDE_EVENT_LISTENERS] || []).map(function (eventListenerBinder) {
-      eventListenerBinder(el, coelem);
     });
-  });
 };
-
-var outsideEventsPlugin = {
-  install: install
-};
+var outsideEventsPlugin = { install: install };
 
 return outsideEventsPlugin;
 
